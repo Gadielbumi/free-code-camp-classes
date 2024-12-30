fe67709e7a507688b2e9a6d39bdfafaeff2e2615
Gadielbumi <165483226+Gadielbumi@users.noreply.github.com>
2024-12-30 22:37:37 +0100

    Add project files.

---
+++ free code camp classes/Program.cs
@@ -0,0 +1,38 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace free_code_camp_classes
+{
+    internal class Programle
+    {
+        static void Main(string[] args)
+        {
+            string welcomMessage = "Welcome on our App";
+            string Devinette = "Try to find a nomber between 0 and 4";
+            Console.WriteLine(welcomMessage+Devinette);
+            while (true)
+            {
+              
+                Console.WriteLine("\n Enter your choice");
+                string choice = Console.ReadLine();
+                int Choice = Convert.ToInt32(choice);
+                int numbToFind = 1;
+                if (numbToFind == Choice)
+                {
+                    Console.WriteLine("Congratulation you have succed");
+                    Console.ReadLine();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Try aigain ");
+                }
+            }
+
+        }
+    }
+}
