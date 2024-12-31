d33af827a8fe2636d9665fc960394d24f2f5cd3d
Gadielbumi <165483226+Gadielbumi@users.noreply.github.com>
2024-12-31 17:33:59 +0100

    i've adjust the probleme with invalid enter

--- free code camp classes/Program.cs
+++ free code camp classes/Program.cs
@@ -13,23 +13,30 @@ namespace free_code_camp_classes
         {
             string welcomMessage = "Welcome on our App";
             string Devinette = "Try to find a nomber between 0 and 4";
-            Console.WriteLine(welcomMessage+Devinette);
+            Console.WriteLine(welcomMessage+'\n'+Devinette);
             while (true)
             {
               
                 Console.WriteLine("\n Enter your choice");
                 string choice = Console.ReadLine();
-                int Choice = Convert.ToInt32(choice);
-                int numbToFind = 1;
-                if (numbToFind == Choice)
+                if (int.TryParse(choice, out int Choice))
                 {
-                    Console.WriteLine("Congratulation you have succed");
-                    Console.ReadLine();
-                    break;
+                    int numbToFind = 1;
+                    if (numbToFind == Choice)
+                    {
+                        Console.WriteLine("Congratulation you have succed");
+                        Console.ReadLine();
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Try aigain ");
+                    }
                 }
                 else
                 {
-                    Console.WriteLine("Try aigain ");
+                    Console.WriteLine("error try a number");
+                    Console.ReadLine();
                 }
             }
 
